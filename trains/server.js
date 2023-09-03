0ef7b18bc8e6b460eb4c285c160534cedd37cb1c
eurus9696 <midhun25302@gmail.com>
2023-09-03 11:45:20 +0530

    trial implementation

--- trains/server.js
+++ trains/server.js
@@ -1,39 +1,113 @@
 const express = require('express');
-const sqlite3 = require('sqlite3');
 const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const db = new sqlite3.Database('train_schedule.db');
-
 app.use(express.json());
 
-// Route to fetch train schedules, seat availability, and pricing
+const getAccessToken = async () => {
+    try {
+        const authData = {
+            companyName: 'Midhun railway',
+            clientID: '33a2b0c0-9c7d-46d8-8470-5516f2759e4c',
+            ownerName: 'Midhun k Mohandas',
+            ownerEmail: '124018035@sastra.ac.in',
+            rollNo: '124018035',
+            clientSecret: 'bQVdUBrRDNSuQIpF',
+        };
+
+        const response = await axios.post('http://20.244.56.144/train/auth', authData);
+        return response.data.access_token;
+    } catch (error) {
+        console.error('Error obtaining access token:', error);
+        throw error;
+    }
+};
+
 app.get('/trains', async (req, res) => {
     try {
-        // Fetch train data from your database
-        const currentTime = new Date();
-        const twelveHoursFromNow = new Date(currentTime.getTime() + 12 * 60 * 60 * 1000);
+        const accessToken = await getAccessToken();
 
-        const query = `
-            SELECT *
-            FROM trains
-            WHERE departure_time >= ? AND departure_time <= ?
-            ORDER BY ac_price ASC, sleeper_seats DESC, departure_time DESC;
-        `;
-
-        db.all(query, [currentTime, twelveHoursFromNow], (err, rows) => {
-            if (err) {
-                throw err;
-            }
-
-            // Send the filtered and sorted train data as the API response
-            res.json(rows);
+        const trainApiResponse = await axios.get('http://20.244.56.144/train/trains', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
         });
+
+        const trainData = trainApiResponse.data;
+
+        const twelveHoursLater = new Date(now.getTime() + 12 * 60 * 60 * 1000); // 12 hours in milliseconds
+
+        const filteredTrains = trainData
+            .filter((train) => {
+                const departureTimeWithDelay = new Date(
+                    now.getTime() +
+                    train.delayedBy * 60 * 1000 + // Convert delay to milliseconds
+                    train.departureTime.Hours * 60 * 60 * 1000 +
+                    train.departureTime.Minutes * 60 * 1000 +
+                    train.departureTime.Seconds * 1000
+                );
+                return departureTimeWithDelay <= twelveHoursLater;
+            })
+            .sort((a, b) => {
+                if (a.price.sleeper === b.price.sleeper) {
+                    if (a.price.AC === b.price.AC) {
+                        if (a.seatsAvailable.sleeper === b.seatsAvailable.sleeper) {
+                            const aDepartureTime = new Date(
+                                now.getTime() +
+                                a.delayedBy * 60 * 1000 +
+                                a.departureTime.Hours * 60 * 60 * 1000 +
+                                a.departureTime.Minutes * 60 * 1000 +
+                                a.departureTime.Seconds * 1000
+                            );
+                            const bDepartureTime = new Date(
+                                now.getTime() +
+                                b.delayedBy * 60 * 1000 +
+                                b.departureTime.Hours * 60 * 60 * 1000 +
+                                b.departureTime.Minutes * 60 * 1000 +
+                                b.departureTime.Seconds * 1000
+                            );
+                            return bDepartureTime - aDepartureTime;
+                        } else {
+                            return b.seatsAvailable.sleeper - a.seatsAvailable.sleeper;
+                        }
+                    } else {
+                        return a.price.AC - b.price.AC;
+                    }
+                } else {
+                    // Sort by sleeper price (ascending)
+                    return a.price.sleeper - b.price.sleeper;
+                }
+            });
+
+        res.json(filteredTrains);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error fetching train data:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.get('/trains/:trainNumber', async (req, res) => {
+    try {
+        // Obtain the Bearer access token
+        const accessToken = await getAccessToken();
+
+        const { trainNumber } = req.params;
+
+        // Fetch details for the specific train using the obtained access token
+        const trainDetailResponse = await axios.get(`http://20.244.56.144/train/trains/${trainNumber}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        const trainDetail = trainDetailResponse.data;
+
+        res.json(trainDetail);
+    } catch (error) {
+        console.error('Error fetching train detail:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
